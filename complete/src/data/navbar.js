9918b1bc02a1badf77c37df4981e41411bac7ad2
Thomas Sankara <thomassankara74@gmail.com>
2021-03-16 11:58:37 +0300

    navigation bar data array

---
+++ complete/src/data/navbar.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const navbar = [
+  {
+    id: uuidv4(),
+    title: "Home",
+    url: "#showcase",
+  },
+  {
+    id: uuidv4(),
+    title: "Fashion",
+    url: "#fashion",
+  },
+  {
+    id: uuidv4(),
+    title: "News",
+    url: "#news",
+  },
+  {
+    id: uuidv4(),
+    title: "Gallery",
+    url: "#gallery",
+  },
+  {
+    id: uuidv4(),
+    title: "Follow",
+    url: "#footer",
+  },
+]
