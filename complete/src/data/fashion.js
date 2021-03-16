d7453126564c0341914d22b3d4ca0b19160c59ed
Thomas Sankara <thomassankara74@gmail.com>
2021-03-16 11:58:23 +0300

    fashion data array

---
+++ complete/src/data/fashion.js
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const fashion = [
+  {
+    id: uuidv4(),
+    title: "John Doe",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    image: "./assets/grid-2.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "John Doe",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    image: "./assets/grid-3.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "John Doe",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    image: "./assets/grid-4.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "John Doe",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    image: "./assets/grid-5.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "John Doe",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    image: "./assets/grid-6.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "John Doe",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+    image: "./assets/grid-7.jpeg",
+  },
+]
