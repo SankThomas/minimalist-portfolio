1e0c945991882d93af888f6a0f00e0015a1a4bb2
Thomas Sankara <thomassankara74@gmail.com>
2021-03-16 11:58:59 +0300

    sidebar icons

---
+++ complete/src/data/sidebar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa"
+
+export const sidebar = [
+  {
+    icon: <FaFacebook />,
+  },
+  {
+    icon: <FaTwitter />,
+  },
+  {
+    icon: <FaInstagram />,
+  },
+  {
+    icon: <FaYoutube />,
+  },
+]
