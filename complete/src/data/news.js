55b8e3e78af02aa799836406a4c2a65fbbdd4a22
Thomas Sankara <thomassankara74@gmail.com>
2021-03-16 11:58:47 +0300

    news data array

---
+++ complete/src/data/news.js
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const articles = [
+  {
+    id: uuidv4(),
+    image: "./assets/image-1.jpeg",
+    title: "Lorem ipsum dolor sit amet",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+    link: "Continue Reading",
+  },
+  {
+    id: uuidv4(),
+    image: "./assets/image-2.jpeg",
+    title: "Lorem ipsum dolor sit amet",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+    link: "Continue Reading",
+  },
+  {
+    id: uuidv4(),
+    image: "./assets/image-3.jpeg",
+    title: "Lorem ipsum dolor sit amet",
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+    link: "Continue Reading",
+  },
+]
+
+export const images = [
+  {
+    id: uuidv4(),
+    image: "./assets/grid-1.jpeg",
+  },
+  {
+    id: uuidv4(),
+    image: "./assets/grid-2.jpeg",
+  },
+  {
+    id: uuidv4(),
+    image: "./assets/image-5.jpeg",
+  },
+  {
+    id: uuidv4(),
+    image: "./assets/showcase-4.jpeg",
+  },
+]
